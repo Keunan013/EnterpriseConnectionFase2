09869cc008ee703173d4ed0da6fbce1a96058c08
Keunan <keunan.carvalho@ambevtech.com.br>
2022-04-19 18:27:29 -0300

    Simplifying code

--- app/operation/fill_out_report.py
+++ app/operation/fill_out_report.py
@@ -1,4 +1,5 @@
 from app.operation.create_report import monthly_report, annual_report
+from app.util.validator import is_january
 
 MONTHS = 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
 NUMBER_OF_MONTHS = len(MONTHS)
@@ -18,17 +19,23 @@ def fill_out_reports() -> None:
     break_even_month = None
     for i in range(NUMBER_OF_MONTHS):
         num_units_sold_month = NUMBER_OF_SALES_PER_MONTH[i]
-        num_units_purchased_month = num_units_sold_month if (i != 0) else INITIAL_STOCK + num_units_sold_month
-        num_units_purchased_year += num_units_purchased_month
         base_cost = BASE_COST_PER_MONTH[i]
-        stock = INITIAL_STOCK + num_units_sold_month
-        monthly_cost = num_units_sold_month * base_cost if (i != 0) else stock * base_cost
+        if is_january(i+1):
+            num_units_purchased_month = INITIAL_STOCK + num_units_sold_month
+            stock = num_units_purchased_month
+            monthly_cost = stock * base_cost
+        else:
+            num_units_purchased_month = num_units_sold_month
+            stock = INITIAL_STOCK + num_units_purchased_month
+            monthly_cost = num_units_purchased_month * base_cost
+        num_units_purchased_year += num_units_purchased_month
         annual_cost += monthly_cost
         monthly_gross_revenue = NUMBER_OF_SALES_PER_MONTH[i] * PRODUCT_VALUE
         monthly_net_revenue = monthly_gross_revenue - monthly_cost
         annual_gross_revenue += monthly_gross_revenue
         annual_net_revenue += monthly_net_revenue
         num_units_sold_year += num_units_sold_month
+        # Showing monthly report
         monthly_report(month=MONTHS[i],
                        stock=stock,
                        base_cost=base_cost,
@@ -40,13 +47,12 @@ def fill_out_reports() -> None:
                        monthly_net_revenue=monthly_net_revenue,
                        annual_net_revenue=annual_net_revenue,
                        product_value=PRODUCT_VALUE)
-        stock -= NUMBER_OF_SALES_PER_MONTH[i]
-        restocking = NUMBER_OF_SALES_PER_MONTH[i + 1] if (i < NUMBER_OF_MONTHS - 1) else 0
+        stock -= num_units_sold_month
+        restocking = NUMBER_OF_SALES_PER_MONTH[i + 1] if (i+1 < NUMBER_OF_MONTHS) else 0
         stock += restocking
-
         if annual_net_revenue >= 0 and (annual_net_revenue - monthly_net_revenue) < 0:
             break_even_month = MONTHS[i]
-
+    # Showing annual report
     annual_report(stock=stock,
                   total_purchased=num_units_purchased_year,
                   total_sale=num_units_sold_year,
